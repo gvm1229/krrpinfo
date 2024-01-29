f79cd5c62c23905a7067661ad009c8142f1ec214
Hojin Jeong <hojini1229@gmail.com>
2024-01-29 14:03:26 +0900

    feat: Eliminate TS, rebuild project (yarn 4 & eslint & husky & netlify & tailwindCSS)

---
+++ .eslintrc.js
@@ -0,0 +1,213 @@
+module.exports = {
+  env: {
+    // 브라우저의 document와 같은 객체 사용 여부
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    // 자바스크립트 버전
+    ecmaVersion: 'latest',
+    // 모듈 export를 위해 import, export를 사용 가능여부를 설정
+    sourceType: 'module',
+    // jsx 허용을 설정
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'plugin:storybook/recommended',
+    'plugin:tailwindcss/recommended',
+  ],
+  settings: {
+    react: {
+      // Tells eslint-plugin-react to automatically detect the version of React to use.
+      version: 'detect',
+    },
+    'import/extensions': [
+      '.js',
+      '.jsx',
+      '.ts',
+      '.tsx',
+    ],
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    },
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'autofix',
+    'import',
+  ],
+  rules: {
+    // 0: disabled, 1: warning, 2: error
+    // --- REACT --- //
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-no-useless-fragment': 0,
+    'react/prop-types': 0,
+    'react/style-prop-object': 0,
+    'react/function-component-definition': 0,
+    // 'react/function-component-definition': [
+    //   'warn',
+    //   { namedComponents: 'arrow-function' },
+    // ],
+    'react/no-unescaped-entities': 0,
+    'react/destructuring-assignment': 0,
+    'react/button-has-type': 0,
+    'react/no-unknown-property': 0,
+    'react/no-array-index-key': 1,
+    'react/require-default-props': 0,
+    'react/jsx-filename-extension': [ // allow JSX in TSX files
+      2,
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    ],
+
+    // --- JSX --- //
+    'jsx-a11y/alt-text': 0,
+    'jsx-a11y/iframe-has-title': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-redundant-roles': 0,
+    'jsx-a11y/media-has-caption': 0,
+    // 'jsx-a11y/label-has-associated-control': 0,
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        labelComponents: ['label'],
+        labelAttributes: ['htmlFor'],
+        controlComponents: ['input'],
+      },
+    ],
+
+    // --- TAILWINDCSS --- //
+    'tailwindcss/no-custom-classname': 'off',
+
+    // --- OTHERS --- //
+    'global-require': 0,
+    'no-console': 0,
+    'no-param-reassign': 0,
+    'no-undef': 1,
+    'no-unused-vars': 1,
+    'no-underscore-dangle': 0,
+    'max-len': 0,
+    'nonblock-statement-body-position': 0,
+    'no-plusplus': 0,
+    'no-continue': 0,
+    'no-shadow': 0,
+    'arrow-parens': [
+      'error',
+      'always',
+    ],
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    'array-bracket-newline': [
+      'error',
+      {
+        multiline: true,
+        minItems: 2,
+      },
+    ],
+    'array-element-newline': [
+      'error',
+      'always',
+    ],
+    'function-paren-newline': [
+      'error',
+      'consistent',
+    ],
+    'function-call-argument-newline': [
+      'error',
+      'consistent',
+    ],
+    quotes: [
+      'error',
+      'single',
+    ],
+    semi: [
+      'error',
+      'always',
+    ],
+    curly: [
+      'error',
+      'multi',
+    ],
+    'import/no-extraneous-dependencies': 0,
+    'prefer-destructuring': 0,
+    // required to allow file extension abbreviation
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+
+    // dangerous, but only way to shut up eslint when using import aliases
+    'import/no-unresolved': 0,
+
+    // https://itnext.io/7-recommended-eslint-rules-for-react-typescript-project-1a22b011b4b5
+    'arrow-body-style': [
+      'error',
+      'as-needed',
+    ],
+    'react/self-closing-comp': [
+      'error',
+      { component: true, html: true },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        pathGroups: [
+          {
+            pattern: '@/**/**',
+            group: 'parent',
+            position: 'before',
+          },
+        ],
+        alphabetize: { order: 'asc' },
+      },
+    ],
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['../'],
+      },
+    ],
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,77 @@
 /** @type {import('tailwindcss').Config} */
+const array100To599 = Array.from(Array(500)).map((_, i) => i + 100);
+const object400To2396px = array100To599.reduce((o, key) => ({ ...o, [key]: `${key * 4}px` }), {});
+const array0To599 = Array.from(Array(600)).map((_, i) => i + 1);
+const object1To2396px = array0To599.reduce((o, key) => ({ ...o, [key]: `${key}px` }), {});
+const array0To49 = Array.from(Array(50)).map((_, i) => i + 1);
+const object1To50rem = array0To49.reduce((o, key) => ({ ...o, [key]: `${key}px` }), {});
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
+  mode: 'jit',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  blocklist: [
+    'container',
+    'collapse',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#e30022',
+        secondary: '#cc0000',
+      },
+      fontFamily: {
+        sans: [
+          'Pretendard Variable',
+          'Pretendard',
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+      fontSize: {
+        title: '2.5rem',
+        subtitle: '1.2rem',
+      },
+      maxWidth: {
+        '8xl': '88rem',
+        '9xl': '96rem',
+      },
+      zIndex: {
+        100: '100',
+      },
+      height: {
+        88: '22rem',
+        ...object400To2396px,
+      },
+      width: {
+        88: '22rem',
+        ...object400To2396px,
+      },
+      scale: {
+        103: '1.03',
+      },
+      maxHeight: object400To2396px,
+      lineHeight: object1To2396px,
+      borderWidth: object1To50rem,
+    },
+    screens: {
+      // default norms
+      sm: { min: '0px', max: '1023px' },
+      md: { min: '1024px' },
+      // custom production norms
+      mobile: { max: '720px' },
+      tablet: { min: '721px', max: '1240px' },
+      desktop: { min: '1241px' },
+      // retina: { min: '1921px' },
     },
   },
   plugins: [],
