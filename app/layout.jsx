9a0576b313db9b5b202b27981a1da07ab2f81f88
Hojin Jeong <hojini1229@gmail.com>
2024-01-29 17:13:29 +0900

    feat: Pretendard Font configured

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,8 @@
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 import React from 'react';
 import '@/styles/globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const pretendard = localFont({ src: '../assets/fonts/PretendardVariable.woff2' });
 
 export const metadata = {
   title: 'KRRP Info',
@@ -26,7 +26,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={pretendard.className}>{children}</body>
     </html>
   );
 }
