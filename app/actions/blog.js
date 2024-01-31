dfd0e82a08ded668b7d0a5935006726c4ff6bcda
Hojin Jeong <hojini1229@gmail.com>
2024-01-31 16:16:52 +0900

    feat: database add / delete with server actions

--- app/actions/blog.js
+++ app/actions/blog.js
@@ -1,3 +1,5 @@
+'use server';
+
 import { ObjectId } from 'mongodb';
 import mongoClient from '@/app/util/db';
 
@@ -5,7 +7,7 @@ const db = (await mongoClient()).db('blog');
 
 export async function getAllPosts() {
   try {
-    return await db.collection('posts').find().toArray();
+    return await db.collection('posts').find().sort({ title: 1 }).toArray();
   } catch (error) {
     // Handle the error, you can log it or throw a custom error
     console.error('Error in getAllPosts:', error);
@@ -20,5 +22,25 @@ export async function getPost(id) {
     // Handle the error, you can log it or throw a custom error
     console.error('Error in getPost:', error);
     throw new Error('Failed to retrieve individual post');
+  }
+}
+
+export async function createPost(formData) {
+  try {
+    await db.collection('posts').insertOne({ title: formData.get('postTitle'), content: formData.get('postContent') });
+  } catch (error) {
+    // Handle the error, you can log it or throw a custom error
+    console.error('Error in createPost:', error);
+    throw new Error('Failed to create post');
+  }
+}
+
+export async function deletePostByTitle(formData) {
+  try {
+    await db.collection('posts').deleteOne({ title: formData.get('postTitle') });
+  } catch (error) {
+    // Handle the error, you can log it or throw a custom error
+    console.error('Error in deletePost:', error);
+    throw new Error('Failed to delete post');
   }
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,25 @@
+import { revalidatePath } from 'next/cache';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { getAllPosts } from '@/app/actions/blog';
+import { createPost, deletePostByTitle, getAllPosts } from '@/app/actions/blog';
 
 export default async function Home() {
   const posts = await getAllPosts();
+
+  async function handleSubmit(formData) {
+    'use server';
+
+    await createPost(formData);
+    revalidatePath('/');
+  }
+
+  async function handleDelete(formData) {
+    'use server';
+
+    await deletePostByTitle(formData);
+    revalidatePath('/');
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -16,7 +31,7 @@ export default async function Home() {
         height={37}
         priority
       />
-      <div className="flex flex-col gap-2">
+      <div className="grid grid-cols-2 gap-4">
         {posts.map((post) => (
           <Link
             href={`/posts/${post._id}`}
@@ -25,12 +40,60 @@ export default async function Home() {
           >
             <h1
               key={post._id}
-              className="rounded-lg bg-blue-500 px-3 py-1.5 text-xl font-semibold text-white hover:cursor-pointer hover:bg-blue-400"
+              className="rounded-lg bg-blue-500 px-3 py-1.5 text-center text-xl font-semibold text-white hover:cursor-pointer hover:bg-blue-400"
             >
               {`To ${post.title} 리디렉트`}
             </h1>
           </Link>
         ))}
+      </div>
+      <div className="flex gap-10">
+        <form
+          action={handleSubmit}
+          className="flex flex-col gap-2"
+        >
+          <label>
+            Post Title:
+            <input
+              type="text"
+              name="postTitle"
+              className="ml-4 text-black"
+            />
+          </label>
+          <label>
+            Post Content:
+            <input
+              type="text"
+              name="postContent"
+              className="ml-4 text-black"
+            />
+          </label>
+          <button
+            type="submit"
+            className="rounded-lg bg-green-500 px-3 py-1.5 text-center text-xl font-semibold text-white hover:cursor-pointer hover:bg-green-400"
+          >
+            Add Post
+          </button>
+        </form>
+        <form
+          action={handleDelete}
+          className="flex flex-col gap-2"
+        >
+          <label>
+            Post Title:
+            <input
+              type="text"
+              name="postTitle"
+              className="ml-4 text-black"
+            />
+          </label>
+          <button
+            type="submit"
+            className="rounded-lg bg-red-500 px-3 py-1.5 text-center text-xl font-semibold text-white hover:cursor-pointer hover:bg-red-400"
+          >
+            Delete Post
+          </button>
+        </form>
       </div>
       <h1 className="text-2xl font-bold">Testing DEV...</h1>
     </main>
