a0e1ae10b529d0e7230318c6124b855311c54745
Hojin <hojini1229@gmail.com>
2024-02-20 23:20:12 +0900

    feat: posts root page to feature all mdx posts & clean home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,6 @@
 import Image from 'next/image';
-import Link from 'next/link';
-import { getAllBlogs } from '@/app/actions/blog';
-import Input1 from '@/src/components/Input1';
-
-export const revalidate = 10;
 
 export default async function Home() {
-  const blogs = await getAllBlogs();
-
   return (
     <main className="container flex h-full flex-col items-center justify-between gap-16 p-8 md:p-24">
       <Image
@@ -18,22 +11,6 @@ export default async function Home() {
         height={37}
         priority
       />
-      <div className="grid gap-4 md:grid-cols-2">
-        {blogs.map((blog) => (
-          <Link
-            href={`/blogs/${blog._id}`}
-            key={blog._id}
-          >
-            <h1
-              key={blog._id}
-              className="rounded-lg bg-blue-500 px-3 py-1.5 text-center text-xl font-semibold text-white hover:cursor-pointer hover:bg-blue-400"
-            >
-              {`To ${blog.title} 리디렉트`}
-            </h1>
-          </Link>
-        ))}
-      </div>
-      <Input1 />
       <h1 className="text-2xl font-bold">Testing DEV...</h1>
     </main>
   );
--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,22 +1,29 @@
-import Image from 'next/image';
+import Link from 'next/link';
+import { getAllPostContents } from '@/app/actions/posts';
 
-export default async function PostsPage() {
+export default async function PostRootPage() {
+  const paths = await getAllPostContents();
+
   return (
-    <main className="container flex h-full flex-col items-center justify-between gap-16 p-8 md:p-24">
-      <Image
-        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-        src="/icons/next.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-        priority
-      />
-      <div className="grid gap-4 md:grid-cols-2">
-        <h1 className="flex w-full justify-center text-center">
-          Empty posts
-        </h1>
+    <main className="container flex h-full flex-col items-center p-8 md:py-12">
+      <h1 className="text-2xl font-bold">Posts List</h1>
+      <div className="mt-8 grid gap-4 md:grid-cols-2">
+        {paths.map((post) => (
+          <div
+            key={post.slug}
+            className="rounded-md bg-primary-foreground p-4"
+          >
+            <p className="text-sm text-primary">{new Date(post.date).toLocaleDateString()}</p>
+            <Link
+              href={`/posts/${post.slug}`}
+              className="text-xl font-semibold"
+            >
+              <h2 className="mt-4 text-violet-600 hover:underline">{post.title}</h2>
+            </Link>
+            <p className="mt-4 text-secondary">{post.subtitle}</p>
+          </div>
+        ))}
       </div>
-      <h1 className="text-2xl font-bold">Testing DEV...</h1>
     </main>
   );
 }
