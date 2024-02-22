b2b70cccc7a048411dcd5f1af4649814837f8334
Hojin Jeong <hojini1229@gmail.com>
2024-02-22 13:27:21 +0900

    feat: shadcn dark mode toggle added to storybook template

--- src/stories/Calendar/Calendar1.stories.jsx
+++ src/stories/Calendar/Calendar1.stories.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Calendar1 from '@/src/components/Calendar/Calendar1';
+import { ThemeProvider } from '@/src/components/DarkMode/theme-provider';
+import { ModeToggle } from '@/src/components/DarkMode/theme-toggle';
 
 export default {
   title: 'Components/Calendar/Calendar1',
@@ -13,9 +15,18 @@ export default {
 };
 
 const Template = (args) => (
-  <div className="container relative flex h-screen w-screen items-center justify-center">
-    <Calendar1 {...args} />
-  </div>
+  <ThemeProvider
+    attribute="class"
+    defaultTheme="system"
+    enableSystem
+  >
+    <div className="container relative flex h-screen w-screen items-center justify-center">
+      <div className="absolute right-0 top-10">
+        <ModeToggle />
+      </div>
+      <Calendar1 {...args} />
+    </div>
+  </ThemeProvider>
 );
 
 export const Default = Template.bind({});
