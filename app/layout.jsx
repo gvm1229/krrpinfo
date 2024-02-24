075b38be76ec466febbd37d11c25b0c608f9fc07
gvm1229 <hojini1229@gmail.com>
2024-02-24 19:02:25 +0900

    🐛 fix: wEIRD FONT WEIGHT FIX via antialiased class

--- app/layout.jsx
+++ app/layout.jsx
@@ -46,7 +46,7 @@ export default function RootLayout({ children }) {
       lang="en"
       suppressHydrationWarning
     >
-      <body className="relative min-h-screen bg-background">
+      <body className="relative min-h-screen bg-background antialiased">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
