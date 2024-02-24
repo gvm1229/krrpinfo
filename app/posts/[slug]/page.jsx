2cbb2765416e3c9da8af6dffd19270711455aa4c
gvm1229 <hojini1229@gmail.com>
2024-02-24 21:12:04 +0900

    ✨ feat: markdown post frontmatter better styling

--- app/posts/[slug]/page.jsx
+++ app/posts/[slug]/page.jsx
@@ -2,6 +2,7 @@ import matter from 'gray-matter';
 import Markdown from 'markdown-to-jsx';
 import { notFound } from 'next/navigation';
 import { getAllPostSlugs, getPostContent } from '@/app/actions/posts';
+import TableOfContents from '@/src/components/Markdown/TableOfContents';
 
 export async function generateStaticParams() {
   const paths = await getAllPostSlugs();
@@ -33,10 +34,11 @@ export default async function PostPage({ params }) {
   const formattedDate = new Date(data.date).toLocaleDateString();
 
   return (
-    <div>
-      <div className="text-left">
-        <h1 className="text-2xl text-slate-600">{data.title}</h1>
-        <p className="mt-2 text-slate-400">{formattedDate}</p>
+    <div className="space-y-8 tablet:space-y-16">
+      <div className="border-b pb-4 text-left">
+        <h1 className="text-4xl font-bold dark:text-slate-400">{data.title}</h1>
+        <p className="mt-2 text-2xl dark:text-slate-500">{data.subtitle}</p>
+        <p className="mt-2 text-xl dark:text-slate-600">{formattedDate}</p>
       </div>
 
       <article className="prose dark:prose-invert">
