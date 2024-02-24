bb0aefda413120656de6c4c167be1b909b9cdd68
gvm1229 <hojini1229@gmail.com>
2024-02-25 00:12:33 +0900

    ✨ feat: posts view page to use the blog component

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import { getAllPosts } from '@/app/actions/posts';
+import Blog from '@/src/components/Blog/Blog';
 
 export default async function PostRootPage() {
   const paths = await getAllPosts();
@@ -9,19 +9,14 @@ export default async function PostRootPage() {
       <h1 className="text-2xl font-bold">Posts List</h1>
       <div className="mt-8 grid gap-4 tablet:grid-cols-2 desktop:grid-cols-3">
         {paths.map((post) => (
-          <div
-            key={post.slug}
-            className="rounded-md bg-slate-200 p-4 dark:bg-slate-800"
-          >
-            <p className="text-sm text-primary">{new Date(post.date).toLocaleDateString()}</p>
-            <Link
-              href={`/posts/${post.slug}`}
-              className="text-xl font-semibold"
-            >
-              <h2 className="mt-4 text-violet-600 hover:underline">{post.title}</h2>
-            </Link>
-            <p className="mt-4 text-primary">{post.subtitle}</p>
-          </div>
+          <Blog
+            key={post.title}
+            thumbnail={post.thumbnail}
+            category="No"
+            date={post.date}
+            title={post.title}
+            toNavigate={`posts/${post.slug}`}
+          />
         ))}
       </div>
     </main>
