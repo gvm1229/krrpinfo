20474bdd237fedcbdc5e9e833ec2196b3ba370d5
gvm1229 <hojini1229@gmail.com>
2024-02-25 02:10:18 +0900

    ✨ feat: new markdown viewer raw html integration

--- src/components/Markdown/MarkdownViewer.jsx
+++ src/components/Markdown/MarkdownViewer.jsx
@@ -1,10 +1,16 @@
 import Markdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 
 export default function MarkdownViewer({ content }) {
   return (
     <article className="prose dark:prose-invert">
-      <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+      >
+        {content}
+      </Markdown>
     </article>
   );
 }
