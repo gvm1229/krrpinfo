173e704941af886aa06c852d846a1e54dfc82152
gvm1229 <hojini1229@gmail.com>
2024-02-24 15:32:16 +0900

    ♻️ refactor: cn no longer used in root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,6 @@ import { siteConfig } from '@/config/site';
 import { ThemeProvider } from '@/src/components/DarkMode/theme-provider';
 import { SiteFooter } from '@/src/components/Footer/SiteFooter';
 import { SiteHeader } from '@/src/components/Header/SiteHeader';
-import { cn } from '@/src/util/utils';
 
 const pretendard = localFont({
   src: '../src/styles/fonts/PretendardVariable.woff2',
