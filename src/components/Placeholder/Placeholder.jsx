d34b0699fa73da7598bcc41171860a08c475a717
Hojin Jeong <hojini1229@gmail.com>
2024-02-26 15:19:34 +0900

    :sparkles: feat: Placeholder margin top

--- src/components/Placeholder/Placeholder.jsx
+++ src/components/Placeholder/Placeholder.jsx
@@ -1,5 +1,5 @@
 const Placeholder = ({ height }) => (
-  <div className={`flex ${height} items-center justify-center bg-slate-100 dark:bg-slate-800`}>
+  <div className={`mt-10 flex ${height} items-center justify-center bg-slate-100 dark:bg-slate-800`}>
     <span className="text-lg font-semibold">
       flex container with a height of
       {' '}
