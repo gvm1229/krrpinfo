d1e8b5a25228d8abdf461c0e848f6b16c9cee62d
Hojin Jeong <hojini1229@gmail.com>
2024-02-26 09:36:47 +0900

    :sparkles: feat: post list fetch successful with contentlayer

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,21 +1,29 @@
-import { getAllPosts } from '@/app/actions/posts';
+import { allPosts } from 'contentlayer/generated';
+import { compareDesc } from 'date-fns';
 import Blog from '@/src/components/Blog/Blog';
+import { formatDate } from '@/src/util/utils';
+
+export const metadata = {
+  title: 'Posts',
+};
 
 export default async function PostRootPage() {
-  const paths = await getAllPosts();
+  const posts = allPosts
+    .filter((post) => post.published)
+    .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
 
   return (
     <main className="container flex h-full flex-col items-center p-8 tablet:py-12">
       <h1 className="text-2xl font-bold">Posts List</h1>
       <div className="mt-8 grid gap-4 tablet:grid-cols-2 desktop:grid-cols-3">
-        {paths.map((post, index) => (
+        {posts.map((post, index) => (
           <Blog
             key={post.title}
             thumbnail={post.thumbnail}
             category="No"
-            date={post.date}
+            date={formatDate(post.date)}
             title={post.title}
-            toNavigate={`posts/${post.slug}`}
+            toNavigate={post.slug}
             isImagePriority={index < 3}
           />
         ))}
