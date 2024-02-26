5cf5870bf040d7b5020ea56eccbcd4983c237ce1
Hojin Jeong <hojini1229@gmail.com>
2024-02-26 12:56:20 +0900

    :sparkles: feat: fallback in posts list if there's no post

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -13,21 +13,29 @@ export default async function PostRootPage() {
 
   return (
     <main className="container flex h-full flex-col items-center p-8 tablet:py-12">
-      <h1 className="text-2xl font-bold">Posts List</h1>
-      <div className="mt-8 grid gap-4 tablet:grid-cols-2 desktop:grid-cols-3">
-        {posts.map((post, index) => (
-          <Blog
-            key={post._id}
-            thumbnail={post.thumbnail}
-            date={post.date}
-            title={post.title}
-            description={post.description}
-            tags={post.tags}
-            toNavigate={post.slug}
-            isImagePriority={index < 3}
-          />
-        ))}
-      </div>
+      {posts?.length ? (
+        <>
+          <h1 className="text-2xl font-bold">포스트 목록</h1>
+          <div className="mt-8 grid gap-4 tablet:grid-cols-2 desktop:grid-cols-3">
+            {posts.map((post, index) => (
+              <Blog
+                key={post._id}
+                thumbnail={post.thumbnail}
+                date={post.date}
+                title={post.title}
+                description={post.description}
+                tags={post.tags}
+                toNavigate={post.slug}
+                isImagePriority={index < 3}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <p className="text-center text-2xl font-bold tablet:text-3xl desktop:text-4xl">
+          작성된 포스트가 없습니다.
+        </p>
+      )}
     </main>
   );
 }
