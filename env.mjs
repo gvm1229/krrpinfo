9cd601ed2a9c4748d586b27e9deba55793832eef
Hojin <hojini1229@gmail.com>
2024-03-01 21:37:00 +0900

    ♻️ refactor: env.mjs formatting

--- env.mjs
+++ env.mjs
@@ -1,5 +1,5 @@
-import {createEnv} from "@t3-oss/env-nextjs"
-import {z} from "zod"
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
 
 export const env = createEnv({
   server: {
@@ -19,4 +19,4 @@ export const env = createEnv({
     MONGODB_URL: process.env.MONGODB_URL,
     NEXON_API_KEY: process.env.NEXON_API_KEY,
   },
-})
+});
