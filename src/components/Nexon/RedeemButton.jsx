f24617d7e49e76f519b99ad98f47495b1a07094b
Hojin <hojini1229@gmail.com>
2024-03-02 02:05:29 +0900

    ✨ feat(nexonredeem): server actions without using proxy success & intuitive messages

--- src/components/Nexon/RedeemButton.jsx
+++ src/components/Nexon/RedeemButton.jsx
@@ -1,154 +1,12 @@
 'use client';
 
 import { useState } from 'react';
+import { handleUnified } from '@/app/actions/couponRedeem';
 
 const RedeemButton = () => {
   const [npaCode, setNpaCode] = useState('');
   const [couponCode, setCouponCode] = useState('');
   const [response, setResponse] = useState(null);
-
-  const handleUserLookup = async (payload) => {
-    try {
-      const response = await fetch('https://mcoupon.nexon.com/kartrush/coupon/api/v1/characters-by-npacode', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify(payload),
-      });
-      const responseData = await response.json();
-      return responseData;
-    } catch (error) {
-      console.error('Error fetching user:', error);
-      setResponse(`Error fetching user: ${error}`);
-      return null;
-    }
-  };
-
-  const handleRedeem = async (payload) => {
-    try {
-      const response = await fetch('https://mcoupon.nexon.com/kartrush/coupon/api/v1/redeem-coupon-by-npacode', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(payload),
-      });
-      const responseData = await response.json();
-      return responseData;
-    } catch (error) {
-      console.error('Error fetching coupon:', error);
-      setResponse(`Error fetching coupon: ${error}`);
-      return null;
-    }
-  };
-
-  const handleUnified = async (npaCode, coupon) => {
-    // const npaCode = '07S0LCI10W02W'; // 본계
-    // const npaCode = '07X0MCW10M027'; // 부계
-    // const coupon = '중요한건잊지않는마음';
-    const region = 'KR';
-
-    try {
-      const payload = { npaCode, coupon, region };
-      const responseData = await handleUserLookup(payload);
-      if (responseData.result)
-        try {
-          const redeemPayload = {
-            coupon,
-            id: responseData.info[0].id,
-            name: responseData.info[0].name,
-            npaCode,
-            region,
-            world: '',
-          };
-          const redeemResponseData = await handleRedeem(redeemPayload);
-          if (redeemResponseData.result)
-            setResponse(`{${responseData.name}} 에게 {${responseData.coupon}} 쿠폰 사용 성공`);
-          else
-            setResponse('이미 사용된 쿠폰 / 잘못된 입력');
-        } catch (error) {
-          console.error('[Unified] Error fetching coupon:', error);
-        }
-      else
-        setResponse('이미 사용된 쿠폰 / 잘못된 입력');
-    } catch (error) {
-      console.error('[Unified] Error fetching user:', error);
-    }
-  };
-
-  const handleUserLookupProxy = async (payload) => {
-    try {
-      const response = await fetch('https://proxy.cors.sh/https://mcoupon.nexon.com/kartrush/coupon/api/v1/characters-by-npacode', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-cors-api-key': 'temp_4b3e164d0edac11bccda267eeffae079',
-        },
-        body: JSON.stringify(payload),
-      });
-      const responseData = await response.json();
-      return responseData;
-    } catch (error) {
-      console.error('Error fetching user:', error);
-      setResponse(`Error fetching user: ${error}`);
-      return null;
-    }
-  };
-
-  const handleRedeemProxy = async (payload) => {
-    try {
-      const response = await fetch('https://proxy.cors.sh/https://mcoupon.nexon.com/kartrush/coupon/api/v1/redeem-coupon-by-npacode', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-cors-api-key': 'temp_4b3e164d0edac11bccda267eeffae079',
-        },
-        body: JSON.stringify(payload),
-      });
-      const responseData = await response.json();
-      return responseData;
-    } catch (error) {
-      console.error('Error fetching coupon:', error);
-      setResponse(`Error fetching coupon: ${error}`);
-      return null;
-    }
-  };
-
-  const handleUnifiedProxy = async (npaCode, coupon) => {
-    // const npaCode = '07S0LCI10W02W'; // 본계
-    // const npaCode = '07X0MCW10M027'; // 부계
-    // const coupon = '중요한건잊지않는마음';
-    const region = 'KR';
-
-    try {
-      const payload = { npaCode, coupon, region };
-      const responseData = await handleUserLookupProxy(payload);
-      if (responseData.result)
-        try {
-          const redeemPayload = {
-            coupon,
-            id: responseData.info[0].id,
-            name: responseData.info[0].name,
-            npaCode,
-            region,
-            world: '',
-          };
-          const redeemResponseData = await handleRedeemProxy(redeemPayload);
-          if (redeemResponseData.result)
-            setResponse(`{${responseData.name}} 에게 {${responseData.coupon}} 쿠폰 사용 성공`);
-          else
-            setResponse('이미 사용된 쿠폰 / 잘못된 입력');
-        } catch (error) {
-          console.error('[Unified] Error fetching coupon:', error);
-        }
-      else
-        setResponse('이미 사용된 쿠폰 / 잘못된 입력');
-    } catch (error) {
-      console.error('[Unified] Error fetching user:', error);
-    }
-  };
 
   return (
     <div className="flex size-full flex-col gap-y-8">
@@ -157,19 +15,6 @@ const RedeemButton = () => {
         {' '}
         KR
       </h2>
-      {/* coupon code input */}
-      <label
-        htmlFor="coupon-code"
-        className="text-2xl font-bold"
-      >
-        쿠폰 코드:
-      </label>
-      <input
-        type="text"
-        onChange={(e) => setCouponCode(e.target.value)}
-        value={couponCode}
-        className="-mt-6 border-1 border-slate-500"
-      />
       {/* npa code input */}
       <label
         htmlFor="npa-code"
@@ -183,26 +28,39 @@ const RedeemButton = () => {
         value={npaCode}
         className="-mt-6 border-1 border-slate-500"
       />
+      {/* coupon code input */}
+      <label
+        htmlFor="coupon-code"
+        className="text-2xl font-bold"
+      >
+        쿠폰 코드:
+      </label>
+      <input
+        type="text"
+        onChange={(e) => setCouponCode(e.target.value)}
+        value={couponCode}
+        className="-mt-6 border-1 border-slate-500"
+      />
       <button
         type="button"
         className="rounded-lg bg-blue-500 px-3 py-1.5 text-center text-xl font-semibold text-white hover:cursor-pointer hover:bg-blue-400"
-        onClick={() => handleUnified(npaCode, couponCode)}
+        onClick={() => handleUnified(npaCode, couponCode).then((data) => setResponse(data))}
         data-message="unified"
       >
         쿠폰 리딤
       </button>
-      <button
-        type="button"
-        className="rounded-lg bg-pink-500 px-3 py-1.5 text-center text-xl font-semibold text-white hover:cursor-pointer hover:bg-pink-400"
-        onClick={() => handleUnifiedProxy(npaCode, couponCode)}
-        data-message="unified"
-      >
-        쿠폰 리딤 (Proxy)
-      </button>
       {response && (
         <div>
           <h2 className="text-2xl font-bold">Response:</h2>
-          <p className="mt-2 text-lg font-medium">{JSON.stringify(response, null, 2)}</p>
+          {response.success ? (
+            <p className="mt-2 text-3xl font-bold text-green-500">
+              {response.message}
+            </p>
+          ) : (
+            <p className="mt-2 text-3xl font-bold text-red-500">
+              {response.message}
+            </p>
+          )}
         </div>
       )}
     </div>
