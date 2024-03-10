212ccecb64031195a7a46c77658f2b265ed6811f
Hojin Jeong <hojini1229@gmail.com>
2024-03-11 01:04:28 +0900

    🐛 fix(redeembtn): response display by default, albeit with dummy data, prevent CLS

--- src/components/Nexon/RedeemButton.jsx
+++ src/components/Nexon/RedeemButton.jsx
@@ -49,20 +49,18 @@ const RedeemButton = () => {
       >
         쿠폰 리딤
       </button>
-      {response && (
-        <div>
-          <h2 className="text-2xl font-bold">Response:</h2>
-          {response.success ? (
-            <p className="mt-2 text-3xl font-bold text-green-500">
-              {response.message}
-            </p>
-          ) : (
-            <p className="mt-2 text-3xl font-bold text-red-500">
-              {response.message}
-            </p>
-          )}
-        </div>
-      )}
+      <div>
+        <h2 className="text-2xl font-bold">Response:</h2>
+        {response?.success ? (
+          <p className="mt-2 text-3xl font-bold text-green-500">
+            {response?.message}
+          </p>
+        ) : (
+          <p className="mt-2 text-3xl font-bold text-red-500">
+            {response?.message ?? '응답이 없습니다.'}
+          </p>
+        )}
+      </div>
     </div>
   );
 };
