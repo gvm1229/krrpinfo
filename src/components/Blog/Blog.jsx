8e8f13594a8bae26554bae5cdc5a8f68c10bc69d
Hojin Jeong <hojini1229@gmail.com>
2024-03-11 11:19:28 +0900

    :sparkles: feat: Blog clickable elements separated & breaking paddings fixed & Use Tag component

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,7 +2,8 @@ import { Link as LinkIcon } from 'lucide-react';
 import Link from 'next/link';
 import ButtonNewTab from '@/src/components/Button/ButtonNewTab';
 import ResponsiveImage from '@/src/components/Image/ResponsiveImage';
-import { formatDate } from '@/src/util/utils';
+import Tag from '@/src/components/Tag/Tag';
+import { cn, formatDate } from '@/src/util/utils';
 
 const Blog = ({
   width,
@@ -15,20 +16,95 @@ const Blog = ({
   hyperlink,
   gridNums = [1, 2, 3],
   isImagePriority = false,
+  className,
 }) => {
-  const children = (
-    <>
-      <div className={`relative aspect-video rounded-md ${width}`}>
-        <div
-          className="absolute inset-0 z-10 rounded-md bg-black opacity-0 transition group-hover:opacity-20 dark:bg-white"
-        />
-        {hyperlink && (
-          <div className="absolute z-20 rounded-br-md rounded-tl-md bg-white/70 p-2">
-            <LinkIcon
-              className="size-8 text-blue-600"
+  const mutualClassName = cn('relative flex flex-col rounded-lg focus:outline-none', className);
+
+  if (toNavigate)
+    return (
+      <div className={mutualClassName}>
+        <Link href={toNavigate}>
+          <div className={`relative aspect-video rounded-md ${width}`}>
+            <div
+              className="absolute inset-0 z-10 rounded-md bg-black opacity-0 transition hover:opacity-20 dark:bg-white"
+            />
+            <ResponsiveImage
+              src={thumbnail}
+              alt="blog-thumbnail"
+              wrapperClassName="rounded-md shadow-md dark:shadow-slate-600"
+              gridNums={gridNums}
+              isPriority={isImagePriority}
             />
           </div>
-        )}
+        </Link>
+        <div className="group mt-4 space-y-1 text-left">
+          <h2
+            className="text-sm font-medium text-neutral-600 dark:text-neutral-200"
+          >
+            {formatDate(date ?? new Date())}
+          </h2>
+          <Link href={toNavigate}>
+            <h1 className="w-fit truncate text-2xl font-bold text-primary hover:underline">{title}</h1>
+          </Link>
+          {description && (
+            <p className="truncate font-medium text-neutral-600 dark:text-neutral-200">{description}</p>
+          )}
+          <Tag
+            tagInput={tags}
+            className="pt-2"
+          />
+        </div>
+      </div>
+    );
+
+  if (hyperlink)
+    return (
+      <div className={mutualClassName}>
+        <ButtonNewTab href={hyperlink}>
+          <div className={`relative aspect-video rounded-md ${width}`}>
+            <div
+              className="absolute inset-0 z-10 rounded-md bg-black opacity-0 transition hover:opacity-20 dark:bg-white"
+            />
+            <div className="absolute z-20 rounded-br-md rounded-tl-md bg-white/70 p-2">
+              <LinkIcon
+                className="size-8 text-blue-600"
+              />
+            </div>
+            <ResponsiveImage
+              src={thumbnail}
+              alt="blog-thumbnail"
+              wrapperClassName="rounded-md shadow-md dark:shadow-slate-600"
+              gridNums={gridNums}
+              isPriority={isImagePriority}
+            />
+          </div>
+        </ButtonNewTab>
+        <div className="group mt-4 space-y-1 text-left">
+          <h2
+            className="text-sm font-medium text-neutral-600 dark:text-neutral-200"
+          >
+            {formatDate(date ?? new Date())}
+          </h2>
+          <ButtonNewTab href={hyperlink}>
+            <h1 className="w-fit truncate text-2xl font-bold text-primary hover:underline">{title}</h1>
+          </ButtonNewTab>
+          {description && (
+            <p className="truncate font-medium text-neutral-600 dark:text-neutral-200">{description}</p>
+          )}
+          <Tag
+            tagInput={tags}
+            className="pt-2"
+          />
+        </div>
+      </div>
+    );
+
+  return (
+    <div className={mutualClassName}>
+      <div className={`relative aspect-video rounded-md ${width}`}>
+        <div
+          className="absolute inset-0 z-10 rounded-md bg-black opacity-0 transition hover:opacity-20 dark:bg-white"
+        />
         <ResponsiveImage
           src={thumbnail}
           alt="blog-thumbnail"
@@ -39,49 +115,15 @@ const Blog = ({
       </div>
       <div className="group mt-4 space-y-1 text-left">
         <h2 className="text-sm font-medium text-neutral-600 dark:text-neutral-200">{formatDate(date ?? new Date())}</h2>
-        <h1 className="truncate text-2xl font-bold text-primary">{title}</h1>
+        <h1 className="w-fit truncate text-2xl font-bold text-primary hover:underline">{title}</h1>
         {description && (
           <p className="truncate font-medium text-neutral-600 dark:text-neutral-200">{description}</p>
         )}
-        <ul className="flex gap-2 pt-2">
-          {tags.map((tag) => (
-            <li
-              key={tag}
-              className="w-fit rounded-lg bg-blue-500 px-2 py-1.5 text-sm font-semibold text-white dark:bg-blue-600"
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
+        <Tag
+          tagInput={tags}
+          className="pt-2"
+        />
       </div>
-    </>
-  );
-
-  const mutualClassName = 'p-4 relative flex flex-col rounded-lg focus:outline-none group transition hover:bg-secondary';
-
-  if (toNavigate)
-    return (
-      <Link
-        href={toNavigate}
-        className={mutualClassName}
-      >
-        {children}
-      </Link>
-    );
-
-  if (hyperlink)
-    return (
-      <ButtonNewTab
-        href={hyperlink}
-        className={mutualClassName}
-      >
-        {children}
-      </ButtonNewTab>
-    );
-
-  return (
-    <div className={mutualClassName}>
-      {children}
     </div>
   );
 };
