a7ca607108ee11c9e25a165dcf3b3defec2d69eb
Hojin Jeong <hojini1229@gmail.com>
2024-03-11 17:28:59 +0900

    :fire: refactor: code styling is needless in this project

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,6 +1,5 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypePrettyCode from 'rehype-pretty-code';
 import rehypeSlug from 'rehype-slug';
 import remarkGfm from 'remark-gfm';
 
@@ -65,24 +64,6 @@ export default makeSource({
     remarkPlugins: [remarkGfm],
     rehypePlugins: [
       rehypeSlug,
-      [
-        rehypePrettyCode,
-        {
-          theme: 'github-dark',
-          onVisitLine(node) {
-            // Prevent lines from collapsing in `display: grid` mode, and allow empty
-            // lines to be copy/pasted
-            if (node.children.length === 0)
-              node.children = [{ type: 'text', value: ' ' }];
-          },
-          onVisitHighlightedLine(node) {
-            node.properties.className.push('line--highlighted');
-          },
-          onVisitHighlightedWord(node) {
-            node.properties.className = ['word--highlighted'];
-          },
-        },
-      ],
       [
         rehypeAutolinkHeadings,
         {
