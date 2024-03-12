557dc53eb6c643503d277ed94d0bb8fc7167748c
Hojin Jeong <hojini1229@gmail.com>
2024-03-12 11:13:12 +0900

    :bug: fix: calendar unused props warning resolve

--- src/components/ui/calendar.jsx
+++ src/components/ui/calendar.jsx
@@ -35,7 +35,7 @@ function Calendar({
           table: 'w-full border-collapse space-y-1',
           head_row: 'flex justify-between',
           head_cell:
-          'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]',
+            'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]',
           row: 'flex w-full justify-between mt-2',
           cell: 'h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
           day: cn(
@@ -44,25 +44,26 @@ function Calendar({
           ),
           day_range_end: 'day-range-end',
           day_selected:
-          'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
+            'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
           day_today: 'bg-accent text-accent-foreground',
           day_outside:
-          'day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30',
+            'day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30',
           day_disabled: 'text-muted-foreground opacity-50',
           day_range_middle:
-          'aria-selected:bg-accent aria-selected:text-accent-foreground',
+            'aria-selected:bg-accent aria-selected:text-accent-foreground',
           day_hidden: 'invisible',
           ...classNames,
         }}
         components={{
-          IconLeft: ({ ...props }) => <ChevronLeft className="size-4" />,
-          IconRight: ({ ...props }) => <ChevronRight className="size-4" />,
+          IconLeft: () => <ChevronLeft className="size-4" />,
+          IconRight: () => <ChevronRight className="size-4" />,
         }}
         {...props}
       />
     )
   );
 }
+
 Calendar.displayName = 'Calendar';
 
 export { Calendar };
