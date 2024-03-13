d5fb663a353d8861bc15b2d19e34b04420b9bb8b
Hojin Jeong <hojini1229@gmail.com>
2024-03-13 17:56:59 +0900

    :bug: fix: route edge runtime syntax fix

--- app/api/viewcount/route.ts
+++ app/api/viewcount/route.ts
@@ -2,10 +2,9 @@ import { Redis } from '@upstash/redis';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+export const runtime = 'edge';
+
 const redis = Redis.fromEnv();
-export const config = {
-  runtime: 'edge',
-};
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
   if (request.method !== 'POST')
@@ -17,24 +16,29 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
   const body = await request.json();
   const slug = body.slug as string | undefined;
 
-  if (!slug)
-    return new NextResponse('Slug not found', { status: 400 });
+  if (!slug) return new NextResponse('Slug not found', { status: 400 });
 
   const ip = request.ip ?? request.headers.get('X-Forwarded-For');
 
   if (ip) {
     // Hash the IP and turn it into a hex string
-    const buf = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(ip));
+    const buf = await crypto.subtle.digest(
+      'SHA-256',
+      new TextEncoder().encode(ip),
+    );
     const hash = Array.from(new Uint8Array(buf))
       .map((b) => b.toString(16).padStart(2, '0'))
       .join('');
 
-    const isNewView = await redis.set(['deduplicate', hash, slug].join(':'), true, {
-      nx: true,
-      ex: 60 * 60,
-    });
-    if (!isNewView)
-      return new NextResponse(null, { status: 202 });
+    const isNewView = await redis.set(
+      ['deduplicate', hash, slug].join(':'),
+      true,
+      {
+        nx: true,
+        ex: 60 * 60,
+      },
+    );
+    if (!isNewView) return new NextResponse(null, { status: 202 });
   }
 
   await redis.incr(['pageviews', 'projects', slug].join(':'));
