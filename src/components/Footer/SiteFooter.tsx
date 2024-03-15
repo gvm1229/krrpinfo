94d2cf1a44a1adfb37d8a017f42cc253343e0101
Hojin Jeong <hojini1229@gmail.com>
2024-03-15 14:43:52 +0900

    :sparkles: feat: footer more concise in mobile & font size unify

--- src/components/Footer/SiteFooter.tsx
+++ src/components/Footer/SiteFooter.tsx
@@ -5,11 +5,17 @@ import { cn } from '@/src/util/utils';
 
 export function SiteFooter({ className, totalViews }) {
   return (
-    <footer id="footer" className={cn(className)}>
-      <div className="flex flex-col items-center py-8 mobile_only:container tablet_only:container tablet:flex-row tablet:justify-between desktop:px-8">
-        <div className="flex flex-col items-start justify-between gap-4">
+    <footer
+      id="footer"
+      className={cn(
+        'py-4 mobile_only:container tablet_only:container tablet:py-8 desktop:px-8',
+        className,
+      )}
+    >
+      <div className="flex flex-col items-center mobile_only:space-y-4 tablet:flex-row tablet:justify-between">
+        <div className="flex flex-col items-center justify-between tablet:items-start tablet:gap-6">
           {/* contact */}
-          <p className="text-center text-sm leading-loose tablet:text-left tablet:text-lg">
+          <p className="text-center leading-loose tablet:text-left tablet:text-lg">
             오류 신고 및 문의 :
             {' '}
             <a
@@ -23,7 +29,7 @@ export function SiteFooter({ className, totalViews }) {
             .
           </p>
           {/* copyright */}
-          <p className="text-center text-sm leading-loose tablet:text-left tablet:text-base">
+          <p className="hidden text-left leading-loose tablet:block">
             {`© ${new Date().getFullYear()} `}
             <a
               href="https://github.com/gvm1229"
@@ -36,16 +42,29 @@ export function SiteFooter({ className, totalViews }) {
             . All rights reserved.
           </p>
         </div>
-        <div className="mt-4 flex flex-col items-center gap-2 tablet:mt-0 tablet:items-end tablet:justify-end">
-          <ViewCounter view={totalViews} />
+        <div className="flex items-center justify-center gap-2 tablet:flex-col tablet:items-end tablet:justify-end tablet:gap-2">
           {/* qr code */}
-          <div className="mt-2 flex items-center gap-2 tablet:mt-0">
-            <p className="text-lg font-medium">공유용 QR 코드:</p>
-            <div className="size-12">
+          <div className="flex items-center gap-2 tablet:mt-0">
+            <p className="font-medium tablet:text-lg">공유용 QR 코드:</p>
+            <div className="size-10 tablet:size-12">
               <Image src={qrc} alt="QR Code" />
             </div>
           </div>
+          <ViewCounter view={totalViews} />
         </div>
+        {/* copyright */}
+        <p className="text-text-center block leading-loose tablet:hidden">
+          {`© ${new Date().getFullYear()} `}
+          <a
+            href="https://github.com/gvm1229"
+            target="_blank"
+            rel="noreferrer"
+            className="font-bold underline underline-offset-4"
+          >
+            Megi
+          </a>
+          . All rights reserved.
+        </p>
       </div>
     </footer>
   );
