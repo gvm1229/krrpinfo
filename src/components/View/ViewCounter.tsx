a6efe8038477a0a178d26d770be463b67a467948
Hojin Jeong <hojini1229@gmail.com>
2024-03-15 14:39:27 +0900

    :sparkles: feat: viewcounter className props added & font size responsive

--- src/components/View/ViewCounter.tsx
+++ src/components/View/ViewCounter.tsx
@@ -1,6 +1,14 @@
-export default function ViewCounter({ view }: { view: number }) {
+import { cn } from '@/src/util/utils';
+
+export default function ViewCounter({
+  view,
+  className,
+}: {
+  view: number;
+  className?: string;
+}) {
   return (
-    <p className="text-lg font-medium">
+    <p className={cn('text-base font-medium tablet:text-lg', className)}>
       {`전체 방문: ${view}회`}
     </p>
   );
