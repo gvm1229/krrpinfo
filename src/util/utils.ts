a1736834a535a1939a47dfbe1c82943649e94951
Hojin Jeong <hojini1229@gmail.com>
2024-03-15 17:43:12 +0900

    :recycle: refactor: utils TS

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -1,21 +1,25 @@
 import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { env } from '@/env.mjs';
+import type { ClassValue } from 'clsx';
 
-export function cn(...inputs) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function absoluteUrl(path) {
+export function absoluteUrl(path: string): string {
   return `${env.NEXT_PUBLIC_APP_URL}${path}`;
 }
 
-export function formatDate(input) {
+export function formatDate(input: string | number | Date): string {
   const date = new Date(input);
   return date.toLocaleDateString();
 }
 
-export function calcStdImageWidth(widthInput, heightInput) {
+export function calcStdImageWidth(
+  widthInput: number,
+  heightInput: number,
+): number {
   const standardHeight = 1080;
 
   // 498 x 375 (before)
