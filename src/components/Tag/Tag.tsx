7c40a924fbb27fa1476f58c825d9cd160e4394c8
Hojin Jeong <hojini1229@gmail.com>
2024-03-17 02:48:10 +0900

    ✨ feat: tag extra small style

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -4,6 +4,7 @@ import { cn } from '@/src/util/utils';
 interface TagProps {
   tagInput: string[];
   isHoverEnabled?: boolean;
+  isExtraSmall?: boolean;
   className?: string;
   innerClassName?: string;
 }
@@ -11,6 +12,7 @@ interface TagProps {
 const Tag = ({
   tagInput,
   isHoverEnabled = true,
+  isExtraSmall = false,
   className,
   innerClassName,
 }: TagProps) => (
@@ -19,7 +21,8 @@ const Tag = ({
       <li
         key={tag}
         className={cn(
-          'w-fit rounded-lg bg-blue-500 px-2 py-1.5 text-sm font-medium text-white dark:bg-blue-600',
+          'w-fit rounded-lg bg-blue-500 font-medium text-white dark:bg-blue-600',
+          `${isExtraSmall ? 'px-1.5 py-1 text-[0.6rem]' : 'px-2 py-1.5 text-sm'}`,
           `${isHoverEnabled && 'hover:bg-blue-400 hover:underline dark:hover:bg-blue-500'}`,
           innerClassName,
         )}
