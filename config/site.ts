f1cd085bcf34d780bed46da1d4cc54d607a60dd8
Hojin Jeong <hojini1229@gmail.com>
2024-03-16 23:41:57 +0900

    ✨ feat(site): site data og image & github link add & TS

---
+++ config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: 'KRRP INFO',
+  description: 'Next14, KRRP',
+  url: 'https://krrpinfo.vercel.app',
+  ogImage: '../app/opengraph-image.webp',
+  links: {
+    // twitter: 'https://twitter.com/shadcn',
+    github: 'https://github.com/gvm1229/krrpinfo',
+  },
+};
