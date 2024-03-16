6dd900aaec3ea91f5eca8637e80f711b8df0f621
Hojin Jeong <hojini1229@gmail.com>
2024-03-17 00:36:49 +0900

    ♻️ refactor: hooks TS

--- src/hooks/use-lock-body.ts
+++ src/hooks/use-lock-body.ts
@@ -4,8 +4,8 @@
 import { useLayoutEffect } from 'react';
 
 export function useLockBody() {
-  useLayoutEffect(() => {
-    const originalStyle = window.getComputedStyle(
+  useLayoutEffect((): (() => void) => {
+    const originalStyle: string = window.getComputedStyle(
       document.body,
     ).overflow;
     document.body.style.overflow = 'hidden';
