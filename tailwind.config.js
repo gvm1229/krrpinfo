7ef84c221f50d86233ae674657f904020961f014
Hojin Jeong <hojini1229@gmail.com>
2024-03-20 17:42:14 +0900

    :fire: refactor: DaisyUI purged, no need for (not pretty) styled component library yet

--- tailwind.config.js
+++ tailwind.config.js
@@ -116,19 +116,5 @@ module.exports = {
       // retina: { min: '1921px' },
     },
   },
-  plugins: [
-    require('tailwindcss-animate'),
-    require('@tailwindcss/typography'),
-    require('daisyui'),
-  ],
-  daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: 'dark', // name of one of the included themes for dark mode
-    base: false, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: 'daisy-', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ':root', // The element that receives theme color CSS variables
-  },
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
 };
