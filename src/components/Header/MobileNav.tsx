2756f754adb0895f66fe6b140f53a608725ba796
Hojin Jeong <hojini1229@gmail.com>
2024-03-21 17:09:25 +0900

    :sparkles: feat: mobile nav hamburger button outline

--- src/components/Header/MobileNav.tsx
+++ src/components/Header/MobileNav.tsx
@@ -20,8 +20,9 @@ export function MobileNav() {
     <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild>
         <Button
-          variant="ghost"
-          className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+          variant="outline"
+          size="icon"
+          className="px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 tablet:hidden"
         >
           <Menu size={28} />
           <span className="sr-only">Toggle Menu</span>
