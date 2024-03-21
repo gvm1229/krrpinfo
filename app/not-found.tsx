39e9587d54cbf5b3e1f7d0dbd569d43dfe4f7769
Hojin Jeong <hojini1229@gmail.com>
2024-03-21 23:12:34 +0900

    🐛 fix(404): not-found page as server component, fixed functionality & design

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,38 +1,43 @@
-'use client';
-
-import Image from 'next/image';
+import { Home } from 'lucide-react';
 import Link from 'next/link';
-import { usePathname, useSelectedLayoutSegments } from 'next/navigation';
 
-export default function Notfound() {
-  const pathname = usePathname();
-  const directoryName = useSelectedLayoutSegments()[0];
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
+export default function NotFound() {
   return (
-    <main className="container flex h-full flex-col items-center justify-between gap-16 p-8 tablet:p-24">
-      <Image
-        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-        src="/icons/next.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-        priority
-      />
-      <h1 className="text-3xl font-semibold text-red-500">
-        404: Not Found
-      </h1>
-      <Link
-        className="rounded-lg bg-green-500 px-3 py-1.5 text-center text-xl font-semibold text-white hover:cursor-pointer hover:bg-green-400"
-        href="/"
-      >
-        Back to home
-      </Link>
-      <h1 className="text-2xl font-bold">
-        Unmapped Directory:
-        <span className="ml-2 text-3xl font-extrabold text-blue-500">
-          {directoryName !== '__DEFAULT__' ? directoryName : pathname.split('/')[1]}
-        </span>
-      </h1>
-    </main>
+    <div className="absolute inset-0 mx-8 flex items-center justify-center">
+      <Card className="flex h-72 w-full flex-col items-center justify-center tablet:h-80 tablet:max-w-xl desktop:h-96 desktop:max-w-3xl">
+        <CardHeader className="space-y-2 text-center tablet:space-y-4">
+          <CardTitle className="text-4xl tablet:text-6xl desktop:text-7xl">404</CardTitle>
+          <h1 className="text-2xl font-medium tracking-tight tablet:text-3xl desktop:text-4xl">
+            페이지를 찾을 수 없음
+          </h1>
+          <CardDescription className="pt-2 text-base tablet:text-xl desktop:text-2xl">
+            잘못된 주소가 입력되었거나,
+            {' '}
+            <br className="inline tablet:hidden" />
+            존재하지 않는 페이지입니다.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="flex justify-center">
+          <Button asChild className="tablet:h-[3.25rem]">
+            <Link
+              href="/"
+              className="flex items-center gap-3 text-lg tablet:text-xl"
+            >
+              <Home className="size-4 tablet:size-6" />
+              홈 화면으로 돌아가기
+            </Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
