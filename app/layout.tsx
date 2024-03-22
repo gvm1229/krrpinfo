b70b1e4675e1f3be07fe6d122782e97fa5a79e15
Hojin Jeong <hojini1229@gmail.com>
2024-03-22 16:32:18 +0900

    :sparkles: feat: Root layout ogImage set using siteConfig

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,12 +36,27 @@ export const metadata = {
     title: siteConfig.name,
     description: siteConfig.description,
     siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
   },
   twitter: {
     card: 'summary_large_image',
     title: siteConfig.name,
     description: siteConfig.description,
-    images: [`${siteConfig.url}/opengraph-image.webp`],
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
     creator: 'Megi',
   },
   // themeColor: [
