75a7e14ac04442fcdda83a04ed17f3f92992a044
Hojin Jeong <hojini1229@gmail.com>
2024-03-22 10:59:27 +0900

    :sparkles: feat: Footer 오류 문의 카톡 버튼 & 오픈프로필 링크

--- src/components/Footer/SiteFooter.tsx
+++ src/components/Footer/SiteFooter.tsx
@@ -1,5 +1,9 @@
+import { MessageCircle } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 import ViewCounter from '@/components//View/ViewCounter';
+import { buttonVariants } from '@/components/ui/button';
 import qrc from '@/public/assets/images/qrcode/qrc-xl-c.png';
 import { cn } from '@/src/util/utils';
 
@@ -18,19 +22,21 @@ export function SiteFooter({
       <div className="container flex flex-col items-start space-y-2 tablet:flex-row tablet:items-center tablet:justify-between desktop:space-y-0">
         <div className="space-y-2 desktop:space-y-4">
           {/* contact */}
-          <p className="text-center leading-loose tablet:text-left tablet:text-lg">
-            오류 신고 및 문의 :
-            {' '}
-            <a
-              href="/"
-              target="_blank"
-              rel="noreferrer"
-              className="font-bold underline underline-offset-4 hover:text-yellow-400"
+          <div className="flex flex-col gap-2 tablet:flex-row tablet:items-center">
+            <p className="text-left leading-loose tablet:text-left tablet:text-lg">
+              오류 신고 및 문의 :
+            </p>
+            <Link
+              href="https://open.kakao.com/me/Megiii"
+              className={cn(
+                buttonVariants(),
+                'flex gap-3 bg-yellow-300 font-bold hover:bg-yellow-200',
+              )}
             >
-              카카오톡 오픈채팅방
-            </a>
-            .
-          </p>
+              <MessageCircle fill="black" />
+              카카오톡 오픈프로필 (Megiii)
+            </Link>
+          </div>
           {/* qr code & visitor count */}
           <div className="flex flex-col items-start justify-center gap-2 desktop:hidden">
             <div className="flex items-center gap-2">
