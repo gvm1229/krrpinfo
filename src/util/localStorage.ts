b92115a211205224281e3f7b3846436e16071f45
Hojin Jeong <hojini1229@gmail.com>
2024-03-22 12:45:05 +0900

    :sparkles: feat: localStorage util

---
+++ src/util/localStorage.ts
@@ -0,0 +1,19 @@
+class LocalStorage {
+  static setItem(key: string, value: string) {
+    if (typeof window !== 'undefined') localStorage.setItem(key, value);
+  }
+
+  // getItem 은 return 을 넣어줘야 함! setItem, removeItem 이랑 다르게 값을 보여주는녀석!
+  static getItem(key: string) {
+    if (typeof window !== 'undefined') return localStorage.getItem(key);
+
+    // window 객체 localStorage, sessionStorage 는 값이 없을때 null
+    return null;
+  }
+
+  static removeItem(key: string) {
+    if (typeof window !== 'undefined') localStorage.removeItem(key);
+  }
+}
+
+export default LocalStorage;
