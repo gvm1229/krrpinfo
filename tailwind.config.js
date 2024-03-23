a7362ee383073c33f13d754c4a665702b501667a
Hojin Jeong <hojini1229@gmail.com>
2024-03-24 00:17:05 +0900

    :lipstick: style: tailwind rounded-xl

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,6 +54,7 @@ module.exports = {
         },
       },
       borderRadius: {
+        xl: '1rem',
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
