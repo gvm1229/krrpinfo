df5c9e8a2459aef7eff13b2cdabd9059571ce3b1
Hojin Jeong <hojini1229@gmail.com>
2024-03-26 09:36:44 +0900

    :test_tube: test: revalidate server action use revalidateTag -> revalidatePath

--- app/actions/revalidate.ts
+++ app/actions/revalidate.ts
@@ -1,7 +1,7 @@
 'use server';
 
-import { revalidateTag } from 'next/cache';
+import { revalidatePath } from 'next/cache';
 
 export default async function revalidate(path: string) {
-  revalidateTag(path);
+  revalidatePath(path);
 }
