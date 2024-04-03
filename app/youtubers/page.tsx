093d4e5cd4bdae94bed5bfe6c594d230bfe4ac0f
Hojin Jeong <hojini1229@gmail.com>
2024-04-03 17:44:16 +0900

    🐛 fix(youtubers): page meta title fix & grid container add

--- app/youtubers/page.tsx
+++ app/youtubers/page.tsx
@@ -1,10 +1,11 @@
 import YouTubeDataInput from '@/components/Video/YouTubeDataInput';
 import { YouTubeModalTrigger } from '@/components/Video/YouTubeModalTrigger';
 import { siteConfig } from '@/config/site';
+import video1 from '@/content/youtubers/루밍밍/w1Rrw2T7Bz4.json';
 
 export const metadata = {
   title: '추천 유튜버 목록',
-  metadataBase: new URL(`${siteConfig.url}/redeem`),
+  metadataBase: new URL(`${siteConfig.url}/youtubers`),
   alternates: {
     canonical: '/',
     languages: {
@@ -16,8 +17,10 @@ export const metadata = {
 
 export default async function RedeemRootPage() {
   return (
-    <main className="container flex h-full flex-col items-center gap-12">
-      <YouTubeModalTrigger />
+    <main className="container relative flex h-full flex-col items-center gap-12">
+      <div className="grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3">
+        <YouTubeModalTrigger videoData={video1} />
+      </div>
       <YouTubeDataInput />
     </main>
   );
