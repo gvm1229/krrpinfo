e6d9ba101d02e78d26c17fb9244c970ddf86883e
Hojin (Tom) Jeong <hojini1229@gmail.com>
2024-04-04 16:08:54 +0900

    :sparkles: feat: Change scrollbar color to be more visible in both light and dark mode

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -40,7 +40,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-[#aeaeb7] dark:bg-[#93939f]" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
