33338053b467ff5a070620db613ace3c3ec10eb4
Hojin (Tom) Jeong <hojini1229@gmail.com>
2024-04-05 15:15:06 +0900

    ✨ feat(blogfeatured): stories props added so that it's properly rendered in test env

--- src/stories/Blog/BlogFeatured.stories.jsx
+++ src/stories/Blog/BlogFeatured.stories.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BlogFeatured from '@/components/Blog/BlogFeatured';
 import { ThemeProvider } from '@/components/DarkMode/theme-provider';
 import { ModeToggle } from '@/components/DarkMode/theme-toggle';
+import thumbnail from '@/public/assets/images/S28/시즌배너.webp';
 
 export default {
   title: 'Components/Blog/BlogFeatured',
@@ -31,5 +32,9 @@ const Template = (args) => (
 
 export const Default = Template.bind({});
 Default.args = {
-  // Define default props here
+  thumbnail,
+  lastEditDate: '4/17/2024',
+  title: '향후 시즌 미리보기',
+  description: '중국 서버는 한국 서버에 비해 약 10개월 (5시즌) 이 앞서있습니다. 미래에는 어떤 카트, 캐릭터 등이 등장할지 미리 알아보세요.',
+  className: 'container',
 };
