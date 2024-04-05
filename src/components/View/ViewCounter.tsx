c1e6e500afdc4a12476d8fdd94d916f799dea639
Hojin (Tom) Jeong <hojini1229@gmail.com>
2024-04-05 09:29:26 +0900

    :sparkles: feat: ViewCounter error trigger

--- src/components/View/ViewCounter.tsx
+++ src/components/View/ViewCounter.tsx
@@ -7,9 +7,12 @@ export default function ViewCounter({
   view: number;
   className?: string;
 }) {
+  // Checking if view is negative
+  const viewContent = view < 0 ? 'error' : `${view}회`;
+
   return (
     <p className={cn('text-base font-medium tablet:text-lg', className)}>
-      {`전체 방문: ${view}회`}
+      {`전체 방문: ${viewContent}`}
     </p>
   );
 }
