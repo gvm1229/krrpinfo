1bc6aa03a0781c0695572fd666113d717c035e6e
Hojin (Tom) Jeong <hojini1229@gmail.com>
2024-04-09 13:45:34 +0900

    ♻️ refactor(fetchyt): add JSDoc & reorder function

--- app/actions/fetchYouTube.ts
+++ app/actions/fetchYouTube.ts
@@ -62,20 +62,13 @@ export type YouTubeChannel = {
   allVideos: YouTubeVideoItem[];
 };
 
-function filteredUrlId(allUrl: string) {
-  if (allUrl.indexOf('/watch') > -1) {
-    const arr = allUrl.replaceAll(/=|&/g, '?').split('?');
-    return arr[arr.indexOf('v') + 1];
-  }
-  if (allUrl.indexOf('/youtu.be') > -1) {
-    const arr = allUrl.replaceAll(/=|&|\//g, '?').split('?');
-    return arr[arr.indexOf('youtu.be') + 1];
-  }
-
-  return null;
-}
-
-export async function getYoutubeData(url: string) {
+/**
+ * Retrieves YouTube video data from the provided URL.
+ *
+ * @param {string} url - The URL of the YouTube video.
+ * @return {YouTubeVideo} The first YouTube video item from the response data.
+ */
+export async function getYoutubeData(url: string): Promise<YouTubeVideoItem> {
   try {
     const id = filteredUrlId(url);
 
@@ -92,4 +85,23 @@ export async function getYoutubeData(url: string) {
   } catch (err) {
     throw new Error(`Error fetching YT Data: ${err}`);
   }
+}
+
+/**
+ * Function to extract the video ID from a YouTube URL.
+ *
+ * @param {string} allUrl - the input URL
+ * @return {string | null} the video ID if found, otherwise null
+ */
+function filteredUrlId(allUrl: string): string | null {
+  if (allUrl.indexOf('/watch') > -1) {
+    const arr = allUrl.replaceAll(/=|&/g, '?').split('?');
+    return arr[arr.indexOf('v') + 1];
+  }
+  if (allUrl.indexOf('/youtu.be') > -1) {
+    const arr = allUrl.replaceAll(/=|&|\//g, '?').split('?');
+    return arr[arr.indexOf('youtu.be') + 1];
+  }
+
+  return null;
 }
