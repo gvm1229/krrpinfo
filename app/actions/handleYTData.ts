031991e809eea3ba167c61aec37d2bc5180a953b
Hojin (Tom) Jeong <hojini1229@gmail.com>
2024-04-11 14:41:19 +0900

    ✨ feat(ytdatahandler): editVideo function added & edit function param name data -> newData

--- app/actions/handleYTData.ts
+++ app/actions/handleYTData.ts
@@ -79,16 +79,16 @@ export async function insertOneChannel(data: YouTubeChannel) {
  * Edit a YouTube channel in the 'channels' collection of the 'youtubers' database.
  *
  * @param {string} id - The ID (YouTube ID, not MongoDB ObjectId) of the channel to be edited
- * @param {YouTubeChannel} data - The updated data for the channel
+ * @param {YouTubeChannel} newData - The updated data for the channel
  * return nothing as this function just performs an action
  */
-export async function editChannel(id: string, data: YouTubeChannel) {
+export async function editChannel(id: string, newData: YouTubeChannel) {
   const db = (await mongoClient()).db('youtubers');
 
   try {
     await db
       .collection('channels')
-      .updateOne({ channelId: id }, { $set: data });
+      .updateOne({ channelId: id }, { $set: newData });
   } catch (error) {
     // Handle the error, you can log it or throw a custom error
     throw new Error(`Failed to edit channel: ${error}`);
@@ -175,6 +175,30 @@ export async function insertOneVideo(channelId: string, videoData: YouTubeVideoI
   } catch (error) {
     // Handle the error, you can log it or throw a custom error
     throw new Error(`Failed to insert video: ${error}`);
+  }
+}
+
+/**
+ * Edit a video for a specific channel in the YouTube database.
+ *
+ * @param {string} channelId - The ID of the channel
+ * @param {string} videoId - The ID of the video to be edited
+ * @param {YouTubeVideoItem} newData - The updated video data
+ * return nothing as this function just performs an action
+ */
+export async function editVideo(channelId: string, videoId: string, newData: YouTubeVideoItem) {
+  const db = (await mongoClient()).db('youtubers');
+
+  try {
+    await db
+      .collection('channels')
+      .updateOne({ channelId }, { $pull: { allVideos: { id: videoId } } });
+    await db
+      .collection('channels')
+      .updateOne({ channelId }, { $push: { allVideos: newData } });
+  } catch (error) {
+    // Handle the error, you can log it or throw a custom error
+    throw new Error(`Failed to edit video: ${error}`);
   }
 }
 
