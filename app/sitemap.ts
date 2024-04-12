4b2a0777aa8033d104768cd771bb907d90febe6e
Hojin (Tom) Jeong <hojini1229@gmail.com>
2024-04-12 17:48:10 +0900

    ✨ feat(site): new domain set & sitemap dynamic generation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,30 +1,58 @@
+import { compareDesc } from 'date-fns';
+import { allYoutubers } from '@/content/youtubers';
+import { env } from '@/env.mjs';
 import type { MetadataRoute } from 'next';
+import { allPosts } from 'contentlayer/generated';
+
+export type Changefreq = 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never';
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const rootPath = env.NEXT_PUBLIC_APP_URL || 'https://kartrushplus.info';
+
+  const posts = allPosts
+    .filter((post) => post.published)
+    .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+
+  const postsRoutes = posts.map((post) => ({
+    url: `${rootPath}${post.slug}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency: 'daily' as Changefreq,
+    priority: 0.8,
+  }));
+
+  const allYTVideos = Object.keys(allYoutubers).flatMap((channelId) => allYoutubers[channelId].allVideos.map((video) => ({
+    url: `${rootPath}/youtubers/${channelId}/${video.id}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency: 'daily' as Changefreq,
+    priority: 0.8,
+  })));
+
   return [
     {
-      url: 'https://krrpinfo.vercel.app',
+      url: 'https://kartrushplus.info',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://krrpinfo.vercel.app/posts',
+      url: 'https://kartrushplus.info/posts',
       lastModified: new Date(),
       changeFrequency: 'daily',
-      priority: 0.8,
+      priority: 0.9,
     },
+    ...postsRoutes,
     {
-      url: 'https://krrpinfo.vercel.app/youtubers',
+      url: 'https://kartrushplus.info/youtubers',
       lastModified: new Date(),
       changeFrequency: 'daily',
-      priority: 0.8,
+      priority: 0.9,
     },
+    ...allYTVideos,
     {
-      url: 'https://krrpinfo.vercel.app/redeem',
+      url: 'https://kartrushplus.info/redeem',
       lastModified: new Date(),
       changeFrequency: 'always',
-      priority: 0.8,
+      priority: 0.9,
     },
   ];
 }
