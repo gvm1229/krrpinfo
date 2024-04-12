db27a5042dd974e634b9cb9eca798840ad6d0c8b
Hojin (Tom) Jeong <hojini1229@gmail.com>
2024-04-12 13:16:22 +0900

    🐛 fix(layout): safari supports svh, so eliminate h-screen fallback

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,7 +119,7 @@ function render(
 ) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="relative h-screen min-h-svh bg-background antialiased">
+      <body className="relative min-h-svh bg-background antialiased">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <ClientLayout>
             <div className="relative flex min-h-svh flex-col">
