2d7c12fa4c2e09a1dc5adf5afd080f7e04804954
Hojin (Tom) Jeong <hojini1229@gmail.com>
2024-04-23 23:14:05 +0900

    ✨ feat(ytvidtabs): 유튭 비디오 탭 client component 로 독립, idx url 에 유동적 반영 성공

---
+++ src/components/Video/YouTubeVideoTabs.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+import Blog from '@/components/Blog/Blog';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@/components/ui/tabs';
+import type { YouTubeVideoItem } from '@/src/types';
+import { categories } from '@/src/types';
+import { fallBackIndex } from '@/src/util/utils';
+
+const YouTubeVideoTabs = ({
+  categorizedAllVideos,
+  nonZeroCategoryKeys,
+  overrideIndex,
+}: {
+  categorizedAllVideos: {
+    [key: string]: YouTubeVideoItem[];
+  };
+  nonZeroCategoryKeys: string[];
+  overrideIndex: number;
+}) => (
+  <Tabs
+    defaultValue={nonZeroCategoryKeys[fallBackIndex(nonZeroCategoryKeys, overrideIndex)]}
+    className="mt-8 w-full laptop:mt-16"
+  >
+    <TabsList className="flex size-full">
+      {categories.map((category) => (
+        <>
+          {categorizedAllVideos[category].length > 0 && (
+            <TabsTrigger
+              key={category}
+              value={category}
+              onClick={() => {
+                // calculate the index of the category in the categoryKeys array
+                const categoryIndex = nonZeroCategoryKeys.indexOf(category);
+
+                // if categoryIndex === 0, then eliminate the query string
+                if (categoryIndex === 0)
+                  window.history.pushState(null, '', `${window.location.pathname}`);
+                else // else, push the query string with the correct index
+                  window.history.pushState(null, '', `?idx=${categoryIndex}`);
+              }}
+              className="h-10 w-full truncate text-base font-medium tablet:h-12 tablet:text-lg laptop:text-xl"
+            >
+              {category}
+            </TabsTrigger>
+          )}
+        </>
+      ))}
+    </TabsList>
+    {categories.map((category) => (
+      <>
+        {categorizedAllVideos[category].length > 0 && (
+          <TabsContent
+            key={category}
+            value={category}
+          >
+            {categorizedAllVideos[category].length > 0 ? (
+              <div className="relative mt-8 grid w-full grid-cols-1 gap-8 tablet:grid-cols-2 laptop:mt-12 laptop:grid-cols-3">
+                {categorizedAllVideos[category].map((video: YouTubeVideoItem, index: number) => (
+                  <Blog
+                    key={video.id}
+                    hyperlink={`https://www.youtube.com/watch?v=${video.id}`}
+                    thumbnail={video.snippet.thumbnails.maxres.url ?? video.snippet.thumbnails.high.url}
+                    isImagePriority={index < 6}
+                    title={video.snippet.title}
+                    description={video.snippet.channelTitle}
+                    date={video.snippet.publishedAt}
+                    tags={[]}
+                  />
+                ))}
+              </div>
+            ) : (
+              <p className="py-20 text-center text-2xl font-bold tablet:text-3xl laptop:py-28 laptop:text-4xl">
+                현재 카테고리에 해당하는 영상이 없습니다.
+              </p>
+            )}
+          </TabsContent>
+        )}
+      </>
+    ))}
+  </Tabs>
+);
+
+export default YouTubeVideoTabs;
